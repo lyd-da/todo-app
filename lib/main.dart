e72f6bf1d1a76a96a89eaeb36218b6b50c3a627c
Lidya Daniel <lidyadaniel77@gmail.com>
2023-03-14 15:47:59 +0300

    check and delete done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,6 +33,9 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool isSelected = false;
+  String input = '';
+  String search = '';
+  List<Task> tasks = [];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -41,147 +46,150 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Colors.blueGrey[50],
         drawer: Drawer(width: 250.0),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              margin: EdgeInsets.all(40),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(40),
-                color: Colors.white,
-              ),
-              child: Row(
-                children: [
-                  Icon(Icons.search),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Search',
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                margin: EdgeInsets.all(40),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.search),
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            search = value;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search',
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
-              child: Text(
-                'All TODOs',
-                style: TextStyle(
-                  fontSize: 23,
-                  fontWeight: FontWeight.bold,
+                  ],
                 ),
               ),
-            ),
-            Card(
-              margin: EdgeInsets.all(10),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)),
-              child: Row(
-                children: [
-                  Checkbox(
-                      value: isSelected,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isSelected = value!;
-                        });
-                        if(isSelected){
-                          
-                        }
-                      }),
-                  Text(
-                    'First Task',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Spacer(),
-                  IconButton(
-                    tooltip: 'Delete',
-                    onPressed: () {
-                      print('Delete clicked');
-                    },
-                    icon: Icon(
-                      Icons.delete,
-                      color: Colors.red,
-                    ),
-                  )
-                ],
+              SizedBox(
+                height: 30,
               ),
-            ),
-            Card(
-              margin: EdgeInsets.all(15),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: CheckboxListTile(
-                title: const Text('Animate Slowly'),
-                onChanged: (bool? value) {},
-                secondary: const Icon(Icons.hourglass_empty),
-                value: false,
-              ),
-            ),
-            // Card(
-            //   margin: EdgeInsets.all(15),
-            //   shape: RoundedRectangleBorder(
-            //       borderRadius: BorderRadius.circular(20)),
-            //   child: Row(
-            //     children: [
-            //       Checkbox(value: false, onChanged: (bool? checked) {}),
-            //       Text(
-            //         'Third Task',
-            //         style: TextStyle(fontSize: 20),
-            //       ),
-            //       Spacer(),
-            //       IconButton(
-            //         onPressed: () {},
-            //         icon: Icon(Icons.delete, color: Colors.red),
-            //       )
-            //     ],
-            //   ),
-            // )
-          ],
-        ),
-        bottomNavigationBar: Container(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          margin: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: Colors.white,
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Add new',
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
+                child: Text(
+                  'All TODOs',
+                  style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              IconButton(
-                iconSize: 40,
-                onPressed: () {},
-                icon: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.pink[100],
-                  ),
-                  width: 50,
-                  height: 50,
-                  child: Icon(
-                    Icons.add,
-                  ),
-                ),
-              )
+              ...tasks
+                  .where((element) => element.task.contains(search))
+                  .map((e) => Card(
+                        margin: EdgeInsets.all(10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Row(
+                          children: [
+                            Checkbox(
+                                value: e.isSelected,
+                                onChanged: (bool? value) {
+                                  final task = tasks.firstWhere(
+                                      (element) => element.id == e.id);
+                                  setState(() {
+                                    task.isSelected = value as bool;
+                                  });
+                                }),
+                            Text(
+                              e.task,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  decoration: e.isSelected
+                                      ? TextDecoration.lineThrough
+                                      : TextDecoration.none),
+                            ),
+                            Spacer(),
+                            IconButton(
+                              tooltip: 'Delete',
+                              onPressed: () {
+                                tasks.removeWhere(
+                                    (element) => element.id == e.id);
+                                setState(() {});
+                              },
+                              icon: Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                            )
+                          ],
+                        ),
+                      ))
             ],
+          ),
+        ),
+        bottomNavigationBar: Padding(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            margin: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    onChanged: (val) {
+                      setState(() {
+                        input = val;
+                        val = '';
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Add new',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  iconSize: 40,
+                  onPressed: () {
+                    tasks.add(Task(id: Random().nextInt(10), task: input));
+                    setState(() {});
+                  },
+                  icon: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      color: Colors.pink[100],
+                    ),
+                    width: 50,
+                    height: 50,
+                    child: Icon(
+                      Icons.add,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+class Task {
+  dynamic id;
+  bool isSelected;
+  String task;
+  Task({required this.id, required this.task, this.isSelected = false});
 }
